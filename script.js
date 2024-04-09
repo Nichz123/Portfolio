ea020effda102cfa9e946ba68d2d66d77e3c2f32
Nicholas <brunernick11@gmail.com>
2024-04-08 23:04:03 -0500

    Drafted Portfolio with content, bassic css, and background image

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+document.querySelector('h1').animate([
+    { transform: 'translateX(0px)' },
+    { transform: 'translateX(100px)' }
+  ], {
+    duration: 1000, // 1 second
+    iterations: Infinity,
+    direction: 'alternate',
+    easing: 'ease-in-out'
+  });
+  */
